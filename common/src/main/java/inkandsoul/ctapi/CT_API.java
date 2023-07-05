b63941a8b0ae0e992ac422dae179da963cc8ab4f
Just-InkSoul <abc@2077.com>
2023-07-06 03:03:58 +0800

    HotFix

--- common/src/main/java/inkandsoul/ctapi/CT_API.java
+++ common/src/main/java/inkandsoul/ctapi/CT_API.java
@@ -1,5 +1,7 @@
 package inkandsoul.ctapi;
 
+import inkandsoul.ctapi.registries.ModMachines;
+import inkandsoul.ctapi.registries.ModRegistries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +9,7 @@ public class CT_API {
 	public static final String MOD_ID = "ctapi";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static void init() {
-		
+		ModMachines.init();
+		ModRegistries.init();
 	}
 }
