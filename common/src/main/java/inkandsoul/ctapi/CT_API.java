a96219c8990b58ab1780ed0ed5bc172411f42f81
Just-InkSoul <abc@2077.com>
2023-07-06 02:29:16 +0800

    HotFix

--- common/src/main/java/inkandsoul/ctapi/CT_API.java
+++ common/src/main/java/inkandsoul/ctapi/CT_API.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CT_API {
-	public static final String MOD_ID = "CTAPI";
+	public static final String MOD_ID = "ctapi";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static void init() {
 		
